67924a62355bbe32c5f913ef2d1fec98a507e682
AVacAdok <avakuang8@gmail.com>
2022-04-05 05:00:38 +0000

    Use loop to find out answer

--- lighthouse-js-fundamentals/loopy.js
+++ lighthouse-js-fundamentals/loopy.js
@@ -11,3 +11,21 @@ while(x<=200){
   }
   x = x +1;
 }
+
+
+const amounts = [61.00, 52.25, 112.99, 5.00]; 
+const total = 0;
+for (let i = 0; i < amounts.length; i++) {
+  total += amounts[i];
+}
+console.log('Order total is: ', total);
+
+
+//newer loop
+/*
+const total = 0;
+for (let amount of amounts) {
+  total += amount;
+}
+console.log('Order total is: ', total);
+*/
