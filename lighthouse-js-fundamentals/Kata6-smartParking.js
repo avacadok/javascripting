f6a0e3498698da21182ed6ac3aaa0bfcb14a5176
AVacAdok <avakuang8@gmail.com>
2022-04-18 04:41:29 +0000

    use function to find the coordinate in an array

--- lighthouse-js-fundamentals/Kata6-smartParking.js
+++ lighthouse-js-fundamentals/Kata6-smartParking.js
@@ -1,19 +1,23 @@
 const whereCanIPark = function (spots, vehicle) {
-  let coordinates = false;
-  for(let i = 0; i < spots.length; i++){
-    for(let j = 0; j < spots[i].length; j++){
-      if (vehicle === "regular" && spots[i][j] === "R"){
-        coordinates = [i, j];
-        break;
-      }else if (vehicle === "small" && (spots[i][j] === "R" || spots[i][j] === "S")){
-        coordinates = [i, j];
-      }else if(vehicle === "motorcycle" && (spots[i][j] === "R" || spots[i][j] === "S" || spots[i][j] === "M")){
-        coordinates = [i, j];
-        break
+  for (let y = 0; y < spots.length; y++){
+    for (let x = 0; x < spots[y].length; x++){
+      const spot = spots[y][x];
+
+      if (vehicle === "regular"){
+        if (spot === "R"){
+          return [x,y];
+        }
+      }else if(vehicle === "small"){
+        if (spot === "R" || spot === "S"){
+          return [x,y];
+        }
+      }else if(vehicle === "motorcycle"){
+        if (spot === "R" || spot === "S" || spot === "M"){
+          return [x,y];
+        }
       }
     }
-  }
-  return coordinates
+  }return false
 };
 
 console.log(whereCanIPark(
